0d99c2832630f21482946c9ed9c0c978d64e7bac
Stasi <cheka008@gmail.com>
2016-03-23 15:50:55 -0700

    finish and test binary search

---
+++ fa_search.py
@@ -0,0 +1,31 @@
+﻿
+def binary_search(a,x,comparer=lambda e1,e2: e1 - e2):
+    head = 0
+    tail = len(a) - 1
+
+    if comparer(x,a[head]) < 0:
+        return False,head
+    elif comparer(x,a[tail]) > 0:
+        return False,tail + 1
+
+    while tail - head > 1:
+        middle = (head + tail) // 2
+
+        distance = comparer(x,a[middle])
+        if distance == 0:
+            return True,middle
+        elif distance < 0: # x< a[middle]
+            tail = middle
+        else:# x> a[middle]
+            head = middle
+
+    if comparer(a[head], x) == 0:
+        return True,head
+    elif comparer(a[tail],x) == 0:
+        return True,tail
+    else:
+        return False,tail # if we insert, the insert position
+
+
+
+
---
+++ test_search.py
@@ -0,0 +1,34 @@
+﻿
+import unittest
+import fa_search
+
+class BinarySearchTest(unittest.TestCase):
+
+    def test_found(self):
+        a = [1,2,3,4,5,6,9,10,18,20]
+
+        for realindex,e in enumerate(a):
+            found,findindex = fa_search.binary_search(a,e)
+            self.assertTrue(found)
+            self.assertEqual(realindex,findindex)
+
+    def test_not_found(self):
+        a = [1,2,3,4,5,6,9,10,18,20]
+
+        found,index = fa_search.binary_search(a,-1)
+        self.assertFalse(found)
+        self.assertEqual(0,index)
+
+        found,index = fa_search.binary_search(a,21)
+        self.assertFalse(found)
+        self.assertEqual(10,index)
+
+        found,index = fa_search.binary_search(a,8)
+        self.assertFalse(found)
+        self.assertEqual(6,index)
+
+        found,index = fa_search.binary_search(a,15)
+        self.assertFalse(found)
+        self.assertEqual(8,index)
+
+
