09219997829cc61322966ea1578995941b5c60d9
Stasi <cheka008@gmail.com>
2016-03-23 16:02:06 -0700

    substitute user-defined comparer

--- test_search.py
+++ test_search.py
@@ -31,4 +31,19 @@ class BinarySearchTest(unittest.TestCase):
         self.assertFalse(found)
         self.assertEqual(8,index)
 
+    def test_userdef_comparer(self):
+        a = [1,2,3,4,5,6,9,10,18,20]
+        ta = [(e,"element%d"%e) for e in a]
 
+        def compare_tuple(x,y):
+            id1,name1 = x
+            id2,name2 = y
+            return id1-id2
+
+        found,index = fa_search.binary_search(ta,(9,"xxxxx"),compare_tuple)
+        self.assertTrue(found)
+        self.assertEqual("element9",ta[index][1])
+
+        found,index = fa_search.binary_search(ta,(8,"yyyy"),compare_tuple)
+        self.assertFalse(found)
+        self.assertEqual(6,index)
