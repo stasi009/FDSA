3323ecfe6a7fcc756b05eae5fb8f9e819262daeb
Stasi <cheka008@gmail.com>
2016-03-23 21:13:45 -0700

    finish and test sort

---
+++ fa_sort.py
@@ -0,0 +1,32 @@
+﻿
+def bubble_sort(a,comparer=lambda x1,x2:x1 - x2):
+    N = len(a)
+    swapped = False
+    for loop_index in xrange(N - 1):# from 0 to N-2
+
+        swapped = False # reset
+
+        for index in xrange(0,N - 1 - loop_index): 
+            if comparer(a[index],a[index + 1]) > 0: # a[index]>a[index+1]
+                temp = a[index]
+                a[index] = a[index + 1]
+                a[index + 1] = temp
+                swapped = True
+
+        if not swapped:
+            return # already sorted, no need to continue
+
+def insert_sort(a,comparer=lambda x1,x2 : x1 - x2):
+    N = len(a)
+    for unsorted_index in xrange(1,N):
+        element_to_sort = a[unsorted_index]
+
+        sorting_index = unsorted_index - 1
+        while sorting_index >= 0 and a[sorting_index] > element_to_sort:
+            a[sorting_index + 1] = a[sorting_index]
+            sorting_index -= 1
+
+        a[sorting_index + 1] = element_to_sort
+
+            
+
