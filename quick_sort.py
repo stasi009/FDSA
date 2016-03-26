4e951578068b5a50daf9b2973b25c9d01efd95ce
Stasi <cheka008@gmail.com>
2016-03-26 08:38:53 -0700

    in-place quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,5 +1,8 @@
-
-def quick_sort(ar):    
+﻿
+def simple_quick_sort(ar):   
+    """
+    assume all elements are unique
+    """ 
     if len(ar) <= 1:
         return
 
@@ -7,7 +10,6 @@ def quick_sort(ar):
     p = ar[0]
     left = []
     right = []
-        
     for index in xrange(1,len(ar)):
         if ar[index] < p: 
             left.append(ar[index])
@@ -35,3 +37,35 @@ def quick_sort(ar):
         
     # ------------------------ print progress
     print " ".join((str(e) for e in ar))
+
+def swap(ar,i,j):
+    temp = ar[i]
+    ar[i] = ar[j]
+    ar[j] = temp 
+
+def partition(ar,low,high):
+    """
+    partition algorithm see: https://www.youtube.com/watch?v=MZaf_9IZCrc
+    """
+    pivot = ar[high]
+    i = low - 1
+    
+    for j in xrange(low,high):# from low to high-1
+        if ar[j] < pivot:
+            i += 1
+            swap(ar,i,j)        
+    swap(ar,i+1,high)
+    
+    print " ".join( (str(e) for e in ar))
+    return i+1
+
+def quick_sort(ar,low,high):
+    """
+    in-place quick sort
+    """
+    if high > low:
+        pindex = partition(ar,low,high)
+        quick_sort(ar,low,pindex-1)
+        quick_sort(ar,pindex+1,high)
+        
+
