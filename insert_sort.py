ef201ca167e39a3c9f737993886b1db1241c3f84
Stasi <cheka008@gmail.com>
2016-03-26 08:35:12 -0700

    add

---
+++ insert_sort.py
@@ -0,0 +1,20 @@
+﻿
+def insert_sort(a,comparer=lambda x1,x2 : x1 - x2):
+    N = len(a)
+    for unsorted_index in xrange(1,N):
+        # need to copy to variable, since a[unsorted_index] will be overwritten soon
+        element_to_sort = a[unsorted_index]
+
+        sorting_index = unsorted_index - 1
+        while sorting_index >= 0 and a[sorting_index] > element_to_sort:
+            a[sorting_index + 1] = a[sorting_index]
+            sorting_index -= 1
+
+        a[sorting_index + 1] = element_to_sort
+
+class SortTest(unittest.TestCase):
+
+    def test_insert_sort(self):
+        a = [4,1,7,5,8,2,6,3,9]
+        insert_sort(a)
+        self.assertEqual([1,2,3,4,5,6,7,8,9],a)
