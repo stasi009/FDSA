b3dcbd8d5572876cb0e4890e7e2854b7835dd6b1
Stasi <cheka008@gmail.com>
2016-03-31 13:58:28 -0700

    save

--- k_smallest.py
+++ k_smallest.py
@@ -1,4 +1,9 @@
 
+def swap(ar,i,j):
+    temp = ar[i]
+    ar[i] = ar[j]
+    ar[j] = temp
+
 def selection_sort_method(ar,k):
     N = len(ar)
 
@@ -16,9 +21,28 @@ def selection_sort_method(ar,k):
 
     return ar[:k]
 
+def selection_not_sort(ar,k):
+    def find_kmax():
+        kmax = 0
+        for i in xrange(1,k):
+            if ar[i] > ar[kmax]:
+                kmax = i
+        return kmax
+
+    kmax = find_kmax()
+    for i in xrange(k,len(ar)):
+        if ar[i] < ar[kmax]:
+            swap(ar,kmax,i)# by now, first k are smallest within the range [0,i]
+            kmax = find_kmax()
+
+    return ar[:k]
+
+
 lst = [54,26,93,17,77,31,44,55,20]
 k = 4
 print selection_sort_method(lst,k)
+print selection_not_sort(lst,k)
+print selection_not_sort( [1, 9, 2, 4, 7, 6, 3],3)
 
 
 
